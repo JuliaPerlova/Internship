7669be97bca0be8113af64edf7510029fea40fb2
Julia Perlova <dobrpsih@gmail.com>
2020-03-25 20:07:48 +0200

    small changes in auth-service

--- backend/apps/api-gateway/src/app.service.ts
+++ backend/apps/api-gateway/src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ClientProxy, Transport, ClientProxyFactory } from '@nestjs/microservices';
-<<<<<<< HEAD
+
 import { TokenService } from '../../token-service/src/token.service';
-||||||| merged common ancestors
-import { MailServiceService } from '../../mail-service/src/mail-service.service';
-=======
->>>>>>> e4260416130bcb4fa3e9a4898bee15d48b885668
+
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -13,13 +10,7 @@ export class AppService {
   private authClient: ClientProxy;
   private userClient: ClientProxy;
 
-<<<<<<< HEAD
   constructor(private readonly tokenService: TokenService) {
-||||||| merged common ancestors
-  constructor(private readonly mailService: MailServiceService) {
-=======
-  constructor() {
->>>>>>> e4260416130bcb4fa3e9a4898bee15d48b885668
     this.authClient = ClientProxyFactory.create({ 
       transport: Transport.TCP, 
       options: {
@@ -27,6 +18,7 @@ export class AppService {
         port: 8080,
       },
     });
+
     this.userClient = ClientProxyFactory.create({
       transport: Transport.TCP,
       options: {
@@ -79,16 +71,8 @@ export class AppService {
   deleteUser(id: string) {
     return this.userClient.send<string>({ cmd: 'deleteUser' }, id);
   }
-<<<<<<< HEAD
 
   deleteToken(id: string) {
     return this.tokenService.deleteAll(id);
   }
-||||||| merged common ancestors
-
-  async sendMail({ id, email, token }) {
-    return await this.mailService.changePass(id, email, token);
-  }
-=======
->>>>>>> e4260416130bcb4fa3e9a4898bee15d48b885668
 }
